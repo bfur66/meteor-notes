369095f14cfad4098e8edb75ec9c3f5e8a1d2789
Brent Furlong <brentfurlong66@gmail.com>
2018-01-24 12:12:56 -0330

    added additional test to notes

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,15 +5,17 @@ import { Notes } from "./notes";
 
 if (Meteor.isServer) {
   describe("notes", function() {
+    const noteOne = {
+      _id: "testNoteId1",
+      title: "My Title",
+      body: "My body for note",
+      updatedAt: 0,
+      userId: "testUserId1"
+    };
+
     beforeEach(function() {
       Notes.remove({});
-      Notes.insert({
-        _id: "testNoteId1",
-        title: "My Title",
-        body: "My body for note",
-        updatedAt: 0,
-        userId: "testUserId1"
-      });
+      Notes.insert(noteOne);
     });
 
     it("should insert new note", function() {
@@ -34,27 +36,55 @@ if (Meteor.isServer) {
     it("should remove note", function() {
       Meteor.server.method_handlers["notes.remove"].apply(
         {
-          userId: "testUserId1"
+          userId: noteOne.userId
         },
-        ["testNoteId1"]
+        [noteOne._id]
       );
 
-      expect(Notes.findOne({ _id: "testNoteId1" })).toNotExist();
+      expect(Notes.findOne({ _id: noteOne._id })).toNotExist();
     });
 
     it("should not remove note if unauthenticated", function() {
       expect(() => {
-        Meteor.server.method_handlers["notes.remove"].apply({}, [
-          "testNoteId1"
-        ]);
+        Meteor.server.method_handlers["notes.remove"].apply({}, [noteOne._id]);
       }).toThrow();
     });
 
     it("should not remove note if invalid _id", function() {
       expect(() => {
         Meteor.server.method_handlers["notes.remove"].apply({
-          userId: "testUserId1"
+          userId: noteOne.userId
         });
+      }).toThrow();
+    });
+
+    it("should update note", function() {
+      const title = "This is an updated title";
+      Meteor.server.method_handlers["notes.update"].apply(
+        {
+          userId: noteOne.userId
+        },
+        [noteOne._id, { title }]
+      );
+
+      const note = Notes.findOne(noteOne._id);
+
+      expect(note.updatedAt).toBeGreaterThan(0);
+      expect(note).toInclude({
+        title,
+        body: noteOne.body
+      });
+    });
+
+    it("should throw error if extra updates provided", function() {
+      expect(() => {
+        const newProperty = "This is not a property of note";
+        Meteor.server.method_handlers["notes.update"].apply(
+          {
+            userId: noteOne.userId
+          },
+          [{ newProperty }]
+        );
       }).toThrow();
     });
   });
