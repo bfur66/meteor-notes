14752a13fc586e031f51bfbe380c4ca8aa320888
Brent Furlong <brentfurlong66@gmail.com>
2018-01-25 11:00:52 -0330

    added testing for logout function

--- imports/ui/PrivateHeader.test.js
+++ imports/ui/PrivateHeader.test.js
@@ -4,17 +4,38 @@ import expect from "expect";
 import Enzyme, { mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-import PrivateHeader from "./PrivateHeader";
+import { PrivateHeader } from "./PrivateHeader";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 if (Meteor.isClient) {
   describe("PrivateHeader", function() {
     it("should set button text to logout", function() {
-      const wrapper = mount(<PrivateHeader title="Test Title" />);
+      const wrapper = mount(
+        <PrivateHeader title="Test Title" handleLogout={() => {}} />
+      );
       const buttonText = wrapper.find("button").text();
 
       expect(buttonText).toBe("Logout");
+    });
+
+    it("should use title prop as h1 text", function() {
+      const title = "Test Title";
+      const wrapper = mount(
+        <PrivateHeader title={title} handleLogout={() => {}} />
+      );
+      const titleText = wrapper.find("h1").text();
+
+      expect(titleText).toBe(title);
+    });
+
+    it("should call handleLogout on click", function() {
+      const spy = expect.createSpy();
+      const wrapper = mount(<PrivateHeader title="Title" handleLogout={spy} />);
+
+      wrapper.find("button").simulate("click");
+
+      expect(spy).toHaveBeenCalled();
     });
   });
 }
