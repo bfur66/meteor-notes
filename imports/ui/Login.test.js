cb8612ab1512a135a0d6570eb64ca55b86ef1f5f
Brent Furlong <brentfurlong66@gmail.com>
2018-01-25 13:37:57 -0330

    implement these tests

--- imports/ui/Login.test.js
+++ imports/ui/Login.test.js
@@ -1,5 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
 import expect from "expect";
 import Enzyme, { mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
@@ -13,10 +14,15 @@ if (Meteor.isClient) {
       const wrapper = shallow(<Login loginWithPassword={() => {}} />);
 
       wrapper.setState({ error });
+      console.log(wrapper.find("p"));
       expect(wrapper.find("p").text()).toBe(error);
 
       wrapper.setState({ error: "" });
       expect(wrapper.find("p").length).toBe(0);
     });
+
+    it("should call loginWithPassword with form data");
+
+    it("should set loginWithPassword callback errors");
   });
 }
